20fb134eba0c4a6ca9a46ced748dcc394234297a
Min <>
2024-06-13 17:39:19 +0900

    refactor: Change package path for user repository in infrastructure folder

--- default-session-login/src/main/java/com/spring/security/infrastructure/user/repository/DefaultUserRepository.java
+++ default-session-login/src/main/java/com/spring/security/infrastructure/user/repository/DefaultUserRepository.java
@@ -1,9 +1,9 @@
-package com.spring.security.infrastructure.repository.user;
+package com.spring.security.infrastructure.user.repository;
 
 import com.spring.security.domain.user.dto.UserRegisterItem;
 import com.spring.security.domain.user.entity.User;
 import com.spring.security.domain.user.repository.UserRepository;
-import com.spring.security.infrastructure.repository.user.inf.UserJpaRepository;
+import com.spring.security.infrastructure.user.repository.inf.UserJpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 public class DefaultUserRepository implements UserRepository {
--- default-session-login/src/main/java/com/spring/security/infrastructure/user/repository/inf/UserJpaRepository.java
+++ default-session-login/src/main/java/com/spring/security/infrastructure/user/repository/inf/UserJpaRepository.java
@@ -1,4 +1,4 @@
-package com.spring.security.infrastructure.repository.user.inf;
+package com.spring.security.infrastructure.user.repository.inf;
 
 import com.spring.security.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
